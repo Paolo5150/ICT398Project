df252fd2e52705665fffca55efba61edf693dcb9
Dylan Green <Dylan@thefunkygibbon.com>
2019-09-24 12:01:57 +0800

    Working on Pathfinding

--- V3/src/Components/PathFinder.cpp
+++ V3/src/Components/PathFinder.cpp
@@ -15,24 +15,29 @@ void PathFinder::GeneratePath(glm::vec3 start, glm::vec3 end)
 	PathNode* startNode = PathFindingManager::Instance().ClosestNodeAt(start.x, start.y, start.z);
 	PathNode* endNode = PathFindingManager::Instance().ClosestNodeAt(end.x, end.y, end.z);
 
+	PathNode* currentNode = startNode;
+
 	std::vector<std::unique_ptr<PathNode>> closed;
 	std::vector<std::unique_ptr<PathNode>> open = PathFindingManager::Instance().pathNodes;
 
+	open.erase(std::find(open.begin(), open.end(), startNode));
+
 	while (open.size() != 0)
 	{
-		for (std::vector<std::unique_ptr<PathNode>>::iterator it = open.begin(); it != open.end(); it++)
+		for (auto it = open.begin(); it != open.end(); it++)
 		{
 			//find least f, assign to q
 			//pop q from open
-			for (/*each of the nodes around q*/)
+			for (unsigned int i = 0; (*it)->neighbors.size(); i++)
 			{
-				if (i == endNode)
+				if ((*it)->neighbors.at(i) == endNode)
 				{
 					return;
 				}
 				else
 				{
-
+					currentNode->neighbors[i]->distanceFromPrevious = glm::length(currentNode->transform.GetPosition() - currentNode->neighbors[i]->transform.GetPosition());
+					currentNode->neighbors[i]->distanceFromTarget = glm::length(endNode->transform.GetPosition() - currentNode->neighbors[i]->transform.GetPosition());
 				}
 			}
 		}
