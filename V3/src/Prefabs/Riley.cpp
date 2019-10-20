88a3a0f59f4ecc6deda7b3448f8b9d242811b1ec
Paolo_X86 <p.ferri1986@gmail.com>
2019-10-20 12:26:49 +0800

    Changed riely speed

--- V3/src/Prefabs/Riley.cpp
+++ V3/src/Prefabs/Riley.cpp
@@ -182,7 +182,7 @@ void Riley::Move()
 				if (!(fabs(angle) < 1.0)) //Tolerance
 					cross = glm::sign(glm::cross(transform.GetLocalFront(), toTarget)).y;
 
-				glm::vec3 move = glm::normalize(nextPos - transform.GetGlobalPosition()) * 4.0f;
+				glm::vec3 move = glm::normalize(nextPos - transform.GetGlobalPosition()) * 8.0f;
 
 				rb->SetVelocity(move);
 				rb->SetAngularVelocity(0, (angle * cross) * 2, 0);
