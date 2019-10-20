6b30291242bf19756f02a28f5526fa57bca9c20d
Dylan Green <Dylan@thefunkygibbon.com>
2019-10-20 20:09:30 +0800

    Added Sand bank and Mountain, Polishing
    BushCourt:
    ~Fixed seams
    
    Sand:
    +Added sand bank around bush court
    
    Mountain:
    +Added mountain in distance

---
+++ V3/src/Prefabs/Mountain.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "..\Core\GameObject.h"
+
+class Mountain : public GameObject
+{
+
+public:
+	Mountain();
+	~Mountain();
+
+	void Update() override;
+	void Start() override;
+
+private:
+
+};
---
+++ V3/src/Prefabs/Sand.cpp
@@ -0,0 +1,42 @@
+#include "pch.h"
+#include "Sand.h"
+#include "..\Utils\ContentManager.h"
+#include "..\Components\BoxCollider.h"
+
+Sand::Sand() : GameObject("Sand")
+{
+	SetIsStatic(true);
+	ContentManager::Instance().GetAsset<Model>("Sand")->PopulateGameObject(this);
+	//transform.SetScale(1.5);
+	SetCullable(false);
+	Material m;
+	m.SetShader(ContentManager::Instance().GetAsset<Shader>("DefaultStatic"));
+	m.Loadtexture(ContentManager::Instance().GetAsset<Texture2D>("Sand"));
+	m.LoadFloat("shininess", 400.0f);
+	m.SetColor(0.5, 0.5, 0.5);
+	//m.Loadtexture(ContentManager::Instance().GetAsset<Texture2D>("BushCourt"), "diffuse0");
+//	m.LoadFloat("shininess", 300.0f);
+	ApplyMaterial(m);
+
+	Material m2NoLight;
+	m2NoLight.SetShader(ContentManager::Instance().GetAsset<Shader>("DefaultStaticNoLight"));
+	m2NoLight.Loadtexture(ContentManager::Instance().GetAsset<Texture2D>("Sand"), "diffuse0");
+	ApplyMaterial(m2NoLight, NOLIGHT);
+
+}
+
+Sand::~Sand()
+{
+
+}
+
+void Sand::Update()
+{
+	GameObject::Update();
+}
+
+void Sand::Start()
+{
+	GameObject::Start(); //This will call start on all the object components, so it's better to leave it as last call when the collider
+						 // has been added.
+}
