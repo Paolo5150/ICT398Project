42f449e5888ed305f7a3405b7894d9ecc1710dd1
Paolo_X86 <p.ferri1986@gmail.com>
2019-10-24 20:36:52 +0800

    More comments

--- V3/src/GUI/Elements/GUIBar.h
+++ V3/src/GUI/Elements/GUIBar.h
@@ -5,12 +5,12 @@
 #include "..\..\Utils\Maths.h"
 
 /**
-* @class GUIText
-* @brief Defines a GUI element for displaying text
+* @class GUIBar
+* @brief Defines a GUI element for displaying a progress bar
 
 * @author Paolo Ferri
 * @version 01
-* @date 15/03/2019
+* @date 15/09/2019
 *
 *
 * @bug No known bugs.
@@ -19,16 +19,12 @@ class GUIBar : public GUIObject
 {
 public:
 	/**
-	* @brief		Creates a GUI text
-	*
-	* @post					The GUIText object is created
-	* @param message		The text to display
-	* @param posX			The X position on the screen
-	* @param posY			The Y position on the screen
-	* @param r				The red channel of the color
-	* @param g				The green channel of the color
-	* @param b				The blue channel of the color
-	* @param isPercentage	Wether the details provided are percentage of the current window
+	* @brief					Creates a GUI bar
+	* @param sizeX				Width
+	* @param sizeY				Height
+	* @param posX				Position X on the screen
+	* @param posY				Position Y on the screen
+	* @param isPercentage		Whether the parameters specified are % of the screen size
 	*/
 	GUIBar(std::string uniqueName, float sizeX = 50, float sizeY = 50, float posX = 0, float posY = 0, bool isPercentage = false) : GUIObject(uniqueName){
 		position.x = posX;
@@ -45,24 +41,38 @@ public:
 	}
 
 	/**
-	* @brief		Destroy the GUIText
+	* @brief		Destroy the GUIBar
 	*
-	* @pre			The GUIText object must exist
-	* @post			The GUIText object is destroyed
+	* @pre			The GUIBar object must exist
+	* @post			The GUIBar object is destroyed
 	*/
 	~GUIBar() {
 
 	};
 
 	/**
-	* @brief		The mtext color
+	* @brief		The bar background color
 	*/
 	glm::vec4 backgorundColor;
+
+	/**
+	* @brief		The bar foreground color
+	*/
 	glm::vec4 foregroundColor;
+
+	/**
+	* @brief		The bar text color
+	*/
 	glm::vec4 textColor;
 
+	/**
+	* @brief		The text message
+	*/
 	std::string text;
 
+	/**
+	* @brief		The percentage (0 - 1) that determines the bar fill
+	*/
 	float percentage;
 
 	ImGuiCol col;
